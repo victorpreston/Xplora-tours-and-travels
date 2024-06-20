7213a2ac06d060d3fb668575d0693ff263477cae
Xpert <112781610+victorpreston@users.noreply.github.com>
2024-06-21 00:56:04 +0300

    feat(backend): prisma orm configuration & backend setup

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { NavbarComponent} from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
